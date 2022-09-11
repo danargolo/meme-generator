47f8d03b6f67d2607ac203334affc3dfc1dec26f
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-11 01:51:17 -0300

    Resolvendo quesitos 2 e 3

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const meme = document.getElementById('text-input');
 
-
-function insert() {
+function insertText() {
   const container = document.getElementById('meme-text');
   container.innerText = meme.value;
 }
 
-meme.addEventListener('input', insert);
+meme.addEventListener('input', insertText);
+
+const imgInsert = document.querySelector('#meme-insert');
+const imgMeme = document.getElementById('meme-image');
+
+function insertImage(ref) {
+  const reader = new FileReader();
+  reader.readAsDataURL(ref.target.files[0]);
+  reader.onloadend = function imgload() {
+    imgMeme.src = reader.result;
+  };
+}
+imgInsert.addEventListener('change', insertImage);
