acd852a837879f2d81592bf2b474cd47424931fa
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-11 19:42:42 -0300

    Resolvendo quesito 7

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const meme = document.getElementById('text-input');
+const box = document.querySelector('#meme-image-container');
 
 function insertText() {
   const container = document.getElementById('meme-text');
@@ -8,7 +9,7 @@ function insertText() {
 meme.addEventListener('input', insertText);
 
 const imgInsert = document.querySelector('#meme-insert');
-const imgMeme = document.getElementById('meme-image');
+const imgMeme = document.querySelector('#meme-image');
 
 function insertImage(ref) {
   const reader = new FileReader();
@@ -18,3 +19,27 @@ function insertImage(ref) {
   };
 }
 imgInsert.addEventListener('change', insertImage);
+
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
+
+fire.addEventListener('click', () => {
+  box.style.border = '3px dashed rgb(255, 0, 0)';
+});
+
+water.addEventListener('click', () => {
+  box.style.border = '5px double rgb(0, 0, 255)';
+});
+
+earth.addEventListener('click', () => {
+  box.style.border = '6px groove rgb(0, 128, 0)';
+});
+
+const img = document.querySelectorAll('.img');
+
+for (let index = 0; index < img.length; index += 1) {
+  img[index].addEventListener('click', (ref) => {
+    imgMeme.src = ref.target.src;
+  });
+}
